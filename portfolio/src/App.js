22031572dd3b2d82dd0f0f0eb509f1727ea58bd6
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-03-30 23:41:37 +0900

    ルーティング（ページ遷移）設定

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,44 +1,25 @@
 import './App.css';
-import { useState } from 'react';
-import Modal from './modal/modal_about';
-import AccountList from './components/accountList/index';
-import { Box, Flex, Link, Text } from '@chakra-ui/react';
-import Work from './routes/work';
-import { Route, Routes } from 'react-router-dom';
-import NoFound from './routes/nofound';
+import Work from './routes/Work';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './routes/NotFound';
+import { Box } from '@chakra-ui/react';
+import Home from './routes/Home';
 
 function App() {
-
-  const [showAboutModal, setshowAboutModal] = useState(false);
-
-  const modalCloseButton = () => setshowAboutModal(false);
-
   return (
+    <>
     <Box className="App">
-      <Routes>
-        <Route path="/work" element={<Work />} />
-        <Route path="*" element={<NoFound/>} />
-      </Routes>
-
-      <Modal show={showAboutModal} modalCloseButton={modalCloseButton} />
-
-      <Box alignItems='center' className="content" position='relative' height='100vh' bgColor='#236e9d' overflow='hidden' display='flex'>
-
-        <Text color='#fff' className="content-sitename" fontSize='14px' fontWeight='bold' display='block' transform='translateY(-50%)' position='absolute' letterSpacing='0.1em' left='30px' top='50%' textTransform='uppercase'>R.YUKI WORKS Portfolio</Text>
-
-        <Flex alignItems='center' className="wrap" display='flex' justifyContent='center' margin='0 auto' width='75%'>
-          <Link className="wrap-work" overflow='hidden' h='70vh' position='relative' w='48%' href="/work">
-            <Text fontSize='120px' fontWeight='bold' position='absolute' left='-48px' top='40px'>WORK</Text>
-          </Link>
-          <Box cursor='pointer' className="wrap-about" overflow='hidden' h='70vh' right='200px' position='relative' w='48%' onClick={() => setshowAboutModal(true)}>
-            <Text fontSize='120px' fontWeight='bold' position='absolute' right='-48px' bottom='40px'>ABOUT</Text>
-          </Box>
-        </Flex>
-
-        <AccountList />
-      </Box>
-
+      <BrowserRouter>
+        
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/*" element={<NotFound/>} />
+          </Routes>
+        
+      </BrowserRouter>
     </Box>
+    </>
   );
 }
 
