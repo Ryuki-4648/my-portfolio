dba97e47abfc1649b5e779c29380b483cc38134e
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-09 21:56:25 +0900

    モーダルのコンポーネントファイルをjsxからtsxに変更

---
+++ portfolio/src/modal/modal_work.tsx
@@ -0,0 +1,14 @@
+import { Box, Flex } from '@chakra-ui/react';
+import React from 'react';
+
+const modalWork = () => {
+
+  return (
+    <>
+      <Box>hoge</Box>
+      <Flex></Flex>
+    </>
+  );
+}
+
+export default modalWork;
