5635b34f3df039e448dbf925cf2af6f8ef0c43f2
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-08-31 15:53:58 +0900

    トップのクラス変更

--- portfolio/src/routes/Home.tsx
+++ portfolio/src/routes/Home.tsx
@@ -1,118 +1,41 @@
 import { Box, useDisclosure, Modal } from '@chakra-ui/react';
 import React from 'react';
-import ModalAbout from '../modal/modal_about';
+import ModalAbout from '../components/ModalAbout';
 import AccountList from '../components/accountList';
 
 function Home() {
 
   const { isOpen, onOpen, onClose } = useDisclosure()
-
-  // Color
-  const mainColor = '#236e9d';
-
   return (
     <>
-      <Box className='App'>
+      <div className='App'>
+        <div className='p-home'>
 
-        <Box
-          alignItems='center'
-          className='content'
-          position='relative' 
-          height='100vh' 
-          bgColor={mainColor} 
-          overflow='hidden' 
-          display='flex'>
+          <p className='p-home__sitename'>r-yuki works Portfolio</p>
 
-          <p className='content-sitename'
-          style={{
-            fontWeight: 'bold',
-            color: '#fff',
-            position: 'absolute',
-            letterSpacing: '0.1em',
-          }}>r-yuki works Portfolio</p>
-
-          <div className='box01'
-          style={{
-            alignItems: 'center',
-            display: 'flex',
-            justifyContent: 'center',
-            margin: '0 auto',
-          }}>
-            <a
-              style={{ overflow: 'hidden', position: 'relative' }}
-              className='wrap-work'
-              href="/work">
-              <p 
-                style={{ 
-                  fontWeight: 'bold', 
-                  position: 'absolute',
-                  writingMode: 'vertical-rl',
-                }}>
-                WORK
-              </p>
+          <div className='p-home__wrap'>
+            <a className='p-home__work' href="/work">
+              <p className='p-home__workText'>WORK</p>
             </a>
-            <div
-              className='wrap-about'
-              style={{
-                border: '1px solid #fff',
-                clipPath: 'polygon(100% 0, 0 100%, 100% 100%)',
-                cursor: 'pointer',
-                overflow: 'hidden',
-                position: 'relative',
-                transition: 'all .3s'
-              }}
-              onClick={onOpen}>
-              <p 
-                style={{ 
-                fontWeight: 'bold',
-                position: 'absolute'
-              }}>ABOUT</p>
+            <div className='p-home__about' onClick={onOpen}>
+              <p className='p-home__aboutText'>ABOUT</p>
             </div>
           </div>
 
           <Modal isOpen={isOpen} onClose={onClose}>
-            {/* <ModalOverlay /> */}
-            <div className='modal-overlay'
-              style={{
-                position: 'fixed',
-                left: '0',
-                top: '0',
-                width: '100vw',
-                height: '100vh',
-                backgroundColor: 'rgba(0, 0, 0, 0.4)',
-                cursor: 'pointer',
-              }}
-              onClick={onClose}></div>
-            <div 
-              className='modal-content'
-              style={{
-                borderRadius: '20px',
-                maxWidth: 'unset',
-                margin: '0 auto',
-                top: '50%',
-                left: '50%',
-                height: 'auto',
-                transform: 'translate(-50%, -50%)',
-                position: 'absolute',
-                zIndex: 100,
-              }}>
+            <div className='p-about__modalOverlay' onClick={onClose}></div>
+            <div className='p-about__modalContent'>
               <ModalAbout />
-              <button
-                style={{
-                  fontWeight: 'bold',
-                  transition: 'all .3s',
-                  position: 'absolute',
-                }}
-                className='modal-button'
-                onClick={onClose}>Close
+              <button className='p-about__modalButton' onClick={onClose}>
+                <span className='p-about__modalButtonText'>×</span>
               </button>
             </div>
           </Modal>
           <AccountList />
           
-        </Box>
+        </div>
 
-      </Box>
+      </div>
     </>
   )
 }
