03c84b1d08ec893afdf2b30bf9d48461f9c545c8
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2025-01-30 22:18:12 +0900

    アカウントリスト修正

--- portfolio/src/components/accountList.tsx
+++ portfolio/src/components/accountList.tsx
@@ -2,41 +2,32 @@ import { Icon } from '@chakra-ui/react';
 import React from 'react';
 import { FaCompass, FaGithub, FaPen, FaPinterest, FaTwitter } from 'react-icons/fa';
 import { MdMail } from 'react-icons/md';
+import { SiQiita } from "react-icons/si";
+import accountData from '../data/account.json';
+
+// ReactIcons名をオブジェクトとしてマッピング
+const iconMap: { [key: string]: React.ElementType } = {
+  FaGithub,
+  FaPinterest,
+  FaCompass,
+  SiQiita,
+  MdMail
+};
 
 export default function AccountList () {
   return (
     <>
       <ul className="c-home__account">
-        <li className='c-home__accountItem'>
-          <a href="https://github.com/Ryuki-4648" target="_blank" rel="noopener noreferrer">
-          <Icon as={FaGithub} />
-          </a>
-        </li>
-        <li className='c-home__accountItem'>
-          <a href="https://www.pinterest.jp/yuki_017/portfolio/" target="_blank" rel="noopener noreferrer">
-          <Icon as={FaPinterest} />
-          </a>
-        </li>
-        {/* <li className='c-home__accountItem'>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <Icon as={FaPen} />
-          </a>
-        </li> */}
-        {/* <li className='c-home__accountItem'>
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <Icon as={FaTwitter} />
-            </a>
-        </li> */}
-        <li className='c-home__accountItem'>
-          <a href="https://connpass.com/user/Ryuki_4648/" target="_blank" rel="noopener noreferrer">
-            <Icon as={FaCompass} />
-          </a>
-        </li>
-        <li className='c-home__accountItem'>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSe5RJxhW1gDHo5JDDYZaLQrQS8Ip2kTloIIk4l9EhR2yrfHUA/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
-            <Icon as={MdMail} />
-          </a>
-        </li>
+        {accountData.map((data) => {
+          const IconComponent = iconMap[data.icon]; // 文字列をアイコンコンポーネントに変換
+          return (
+            <li key={data.id} className='c-home__accountItem'>
+              <a href={data.url}>
+                {IconComponent ? <Icon as={IconComponent} /> : null}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
